954a496333f72532319c0e634af66d26e74fac8c
SanchoPanzsa <zelgadis4@gmail.com>
2022-03-23 02:07:20 +0300

    Task #2

--- src/io/sanchopansa/HomeworkClass.java
+++ src/io/sanchopansa/HomeworkClass.java
@@ -2,6 +2,14 @@ package io.sanchopansa;
 
 public class HomeworkClass {
     public static void main(String[] args) {
-
+        // Примитивные типы
+        byte byteVar = 5;
+        short shortVar = 10;
+        int intVar = 15;
+        long longVar = 20;
+        float floatVar = 0.5f;
+        double doubleVar = 0.25;
+        char charVar = 'a';
+        boolean booleanVar = true;
     }
 }
